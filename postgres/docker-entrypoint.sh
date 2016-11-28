d2521f53487172bd3922d0126134618c7fb8d956
Andy Daniels <andy@andah.me>
2016-11-27 20:13:05 -0600

    Add psql option to entrypoint that injects host and port (if available), otherwise drops to command override option.

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -7,6 +7,10 @@ if [[ $1 == initdb ]]; then
 
     sed -i -r "s/^(host.*)127\.0\.0\.1\/32(.*)$/\10\.0\.0\.0\/0   \2/g" /etc/postgresql/${PG_MAJOR}/main/pg_hba.conf
     sed -i -r "s/^#(listen_addresses = ')[0-9A-Za-z\.]*(').*$/\1\*\2/g" /etc/postgresql/${PG_MAJOR}/main/postgresql.conf
+elif [ "$1" == "psql" -a "${DB_PORT_5432_TCP_ADDR}" -a "${DB_PORT_5432_TCP_PORT}" ]; then
+    shift
+
+    exec psql -h ${DB_PORT_5432_TCP_ADDR} -p ${DB_PORT_5432_TCP_PORT} $@
 elif [[ $1 == single ]]; then
     exec /usr/lib/postgresql/${PG_MAJOR}/bin/postgres --single -c config-file=/etc/postgresql/${PG_MAJOR}/main/postgresql.conf
 elif [[ $# > 0 ]]; then
