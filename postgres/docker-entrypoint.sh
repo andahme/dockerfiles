59160e29c7501948329f0e98aa43704bb362429f
Andy Daniels <andy@andah.me>
2016-11-27 20:10:55 -0600

    Add single (user mode) option to entrypoint.

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -7,6 +7,8 @@ if [[ $1 == initdb ]]; then
 
     sed -i -r "s/^(host.*)127\.0\.0\.1\/32(.*)$/\10\.0\.0\.0\/0   \2/g" /etc/postgresql/${PG_MAJOR}/main/pg_hba.conf
     sed -i -r "s/^#(listen_addresses = ')[0-9A-Za-z\.]*(').*$/\1\*\2/g" /etc/postgresql/${PG_MAJOR}/main/postgresql.conf
+elif [[ $1 == single ]]; then
+    exec /usr/lib/postgresql/${PG_MAJOR}/bin/postgres --single -c config-file=/etc/postgresql/${PG_MAJOR}/main/postgresql.conf
 elif [[ -e /var/lib/postgresql/${PG_MAJOR}/main/PG_VERSION ]]; then
     exec /usr/lib/postgresql/${PG_MAJOR}/bin/postgres -c config-file=/etc/postgresql/${PG_MAJOR}/main/postgresql.conf
 else
