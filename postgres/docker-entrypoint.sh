389976074301bb5f2565a8073586357cea31d5fd
Andy Daniels <andy@andah.me>
2016-11-27 20:08:47 -0600

    Alter pg_hba.conf via set instead of appending line.  Bash style cleanup.

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
-if [ "$1" == "initdb" ]; then
+if [[ $1 == initdb ]]; then
     shift
 
     pg_createcluster ${PG_MAJOR} main -- --pwprompt $@
 
+    sed -i -r "s/^(host.*)127\.0\.0\.1\/32(.*)$/\10\.0\.0\.0\/0   \2/g" /etc/postgresql/${PG_MAJOR}/main/pg_hba.conf
     sed -i -r "s/^#(listen_addresses = ')[0-9A-Za-z\.]*(').*$/\1\*\2/g" /etc/postgresql/${PG_MAJOR}/main/postgresql.conf
-
-    echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/${PG_MAJOR}/main/pg_hba.conf
-
-    echo Creation complete.
-elif [ -e "/var/lib/postgresql/${PG_MAJOR}/main/PG_VERSION" ]; then
+elif [[ -e /var/lib/postgresql/${PG_MAJOR}/main/PG_VERSION ]]; then
     exec /usr/lib/postgresql/${PG_MAJOR}/bin/postgres -c config-file=/etc/postgresql/${PG_MAJOR}/main/postgresql.conf
 else
     echo Database not found.  Starting interactive shell...
