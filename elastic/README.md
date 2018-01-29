4c9f91050d26e327498e13e2ab6a5bf23b027049
Andy Daniels <andy@andah.me>
2018-01-29 02:16:33 -0600

    Update elastic Dockerfile, docker-entrypoint.sh and README.md

--- elastic/README.md
+++ elastic/README.md
@@ -3,6 +3,5 @@
 ### Elastic Server
 docker run -d --name elastic \
     --network ${NETWORK} --network-alias elastic \
-    -e ELASTIC_NETWORK_HOST=elastic \
     andahme/elastic
 
--- elastic/docker-entrypoint.sh
+++ elastic/docker-entrypoint.sh
@@ -1,31 +1,15 @@
 #!/bin/bash -e
 
-ELASTIC_CONFIG=${ELASTIC_HOME}/config/elasticsearch.yml
+echo "ANDAHME: Applying Configuration (cluster.name): ${ELASTIC_CLUSTER_NAME}"
+echo "ANDAHME: Applying Configuration (network.host): ${ELASTIC_NETWORK_HOST}"
+echo "ANDAHME: Applying Configuration (node.name): ${ELASTIC_NODE_NAME}"
 
-
-if [ ${ELASTIC_CLUSTER_NAME} ]; then
-    echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (cluster.name): ${ELASTIC_CLUSTER_NAME}"
-    sed -i -r "s|^#?(cluster.name:\ ).*|\1${ELASTIC_CLUSTER_NAME}|" ${ELASTIC_CONFIG}
-fi
-
-if [ ${ELASTIC_NODE_NAME} ]; then
-    echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (node.name): ${ELASTIC_NODE_NAME}"
-    sed -i -r "s|^#?(node.name:\ ).*|\1${ELASTIC_NODE_NAME}|" ${ELASTIC_CONFIG}
-fi
-
-echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (path.data): ${ELASTIC_PATH_DATA}"
-sed -i -r "s|^#?(path.data:\ ).*|\1${ELASTIC_PATH_DATA}|" ${ELASTIC_CONFIG}
-
-echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (path.logs): ${ELASTIC_PATH_LOGS}"
-sed -i -r "s|^#?(path.logs:\ ).*|\1${ELASTIC_PATH_LOGS}|" ${ELASTIC_CONFIG}
-
-echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (network.host): ${ELASTIC_NETWORK_HOST:=$(hostname)}}"
-sed -i -r "s|^#?(network.host:\ ).*|\1${ELASTIC_NETWORK_HOST}|" ${ELASTIC_CONFIG}
-
-if [ ${ELASTIC_HTTP_PORT} ]; then
-    echo "ANDAHME: Applying Configuration (${ELASTIC_CONFIG}) - (http.port): ${ELASTIC_HTTP_PORT}"
-    sed -i -r "s|^#?(http.port:\ ).*|\1${ELASTIC_HTTP_PORT}|" ${ELASTIC_CONFIG}
-fi
+sed -i -r \
+  -e "s|^#?(cluster.name:\ ).*|\1${ELASTIC_CLUSTER_NAME}|" \
+  -e "s|^#?(network.host:\ ).*|\1${ELASTIC_NETWORK_HOST}|" \
+  -e "s|^#?(node.name:\ ).*|\1${ELASTIC_NODE_NAME}|" \
+  -e "s|^#?(path.data:\ ).*|\1/data|" \
+  ${ELASTIC_HOME}/config/elasticsearch.yml
 
 
 exec $@
