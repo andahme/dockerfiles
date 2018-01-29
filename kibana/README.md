bc40cba61f3ece45c2baa31fb19b22060049856e
Andy Daniels <andy@andah.me>
2018-01-29 02:19:04 -0600

    Update kibana Dockerfile, docker-entrypoint.sh and README.md

--- kibana/README.md
+++ kibana/README.md
@@ -2,19 +2,16 @@
 
 ### Start Elastic
 ```bash
-docker run -d --rm --name elastic \
+docker run -d --name elastic \
   --network ${NETWORK} --network-alias elastic \
-  -e ELASTIC_NETWORK_HOST=elastic \
   andahme/elastic
-``
+```
 
 ### Start Kibana
 ```bash
-docker run -d --rm --name kibana \
+docker run -d --name kibana \
   --network ${NETWORK} --network-alias kibana \
   -p 127.0.0.1:5601:5601 \
-  -e KIBANA_ELASTICSEARCH_URL=http://elastic:9200 \
-  -e KIBANA_SERVER_HOST=kibana \
   andahme/kibana
 ```
 
--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -1,24 +1,14 @@
 #!/bin/bash -e
 
-KIBANA_CONFIG=${KIBANA_HOME}/config/kibana.yml
+echo "ANDAHME: Applying Configuration (elasticsearch.url): ${KIBANA_ELASTICSEARCH_URL}"
+echo "ANDAHME: Applying Configuration (server.host): ${KIBANA_SERVER_NAME}"
+echo "ANDAHME: Applying Configuration (server.name): ${KIBANA_SERVER_NAME}"
 
-if [ ${KIBANA_SERVER_NAME} ]; then
-    echo "ANDAHME: Applying Configuration (${KIBANA_CONFIG}) - (server.name): ${KIBANA_SERVER_NAME}"
-    sed -i -r "s|^#?(server.name:\ ).*|\1${KIBANA_SERVER_NAME}|" ${KIBANA_CONFIG}
-fi
-
-if [ ${KIBANA_ELASTICSEARCH_URL} ]; then
-    echo "ANDAHME: Applying Configuration (${KIBANA_CONFIG}) - (elasticsearch.url): ${KIBANA_ELASTICSEARCH_URL}"
-    sed -i -r "s|^#?(elasticsearch.url:\ ).*|\1${KIBANA_ELASTICSEARCH_URL}|" ${KIBANA_CONFIG}
-fi
-
-echo "ANDAHME: Applying Configuration (${KIBANA_CONFIG}) - (server.host): ${KIBANA_SERVER_HOST:=$(hostname)}"
-sed -i -r "s|^#?(server.host:\ ).*|\1${KIBANA_SERVER_HOST}|" ${KIBANA_CONFIG}
-
-if [ ${KIBANA_SERVER_PORT} ]; then
-    echo "ANDAHME: Applying Configuration (${KIBANA_CONFIG}) - (server.port): ${KIBANA_SERVER_PORT}"
-    sed -i -r "s|^#?(server.port:\ ).*|\1${KIBANA_SERVER_PORT}|" ${KIBANA_CONFIG}
-fi
+sed -i -r \
+  -e "s|^#?(elasticsearch.url:\ ).*|\1${KIBANA_ELASTICSEARCH_URL}|" \
+  -e "s|^#?(server.host:\ ).*|\1${KIBANA_SERVER_HOST}|" \
+  -e "s|^#?(server.name:\ ).*|\1${KIBANA_SERVER_NAME}|" \
+  ${KIBANA_HOME}/config/kibana.yml
 
 
 exec $@
