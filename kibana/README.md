add8adb0999d8facc3ce864a1e3972ada246b9a4
Andy Daniels <andy@andah.me>
2018-02-23 02:00:10 -0600

    Bump kibana version (6.2.2), update Dockerfile, docker-entrypoint.sh, README.md

--- kibana/README.md
+++ kibana/README.md
@@ -3,14 +3,14 @@
 ### Start Elastic
 ```bash
 docker run -d --name elastic \
-  --network ${NETWORK} --network-alias elastic \
+  --network ${NETWORK} \
   andahme/elastic
 ```
 
 ### Start Kibana
 ```bash
 docker run -d --name kibana \
-  --network ${NETWORK} --network-alias kibana \
+  --network ${NETWORK} \
   -p 127.0.0.1:5601:5601 \
   andahme/kibana
 ```
--- kibana/docker-entrypoint.sh
+++ kibana/docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-echo "ANDAHME: Applying Configuration (elasticsearch.url): ${KIBANA_ELASTICSEARCH_URL}"
-echo "ANDAHME: Applying Configuration (server.host): ${KIBANA_SERVER_NAME}"
-echo "ANDAHME: Applying Configuration (server.name): ${KIBANA_SERVER_NAME}"
+if [ ${KIBANA_SERVER_NAME} ]; then
+  echo "ANDAHME: Applying Configuration (server.name): ${KIBANA_SERVER_NAME}"
+  sed -i -r "s|^#?(server.name:\ ).*|\1${KIBANA_SERVER_NAME}|" ${KIBANA_HOME}/config/kibana.yml
+fi
 
+echo "ANDAHME: Applying Configuration (elasticsearch.url): ${KIBANA_ELASTICSEARCH_URL:=http://elastic:9200}"
 sed -i -r \
   -e "s|^#?(elasticsearch.url:\ ).*|\1${KIBANA_ELASTICSEARCH_URL}|" \
-  -e "s|^#?(server.host:\ ).*|\1${KIBANA_SERVER_HOST}|" \
-  -e "s|^#?(server.name:\ ).*|\1${KIBANA_SERVER_NAME}|" \
+  -e "s|^#?(server.host:\ ).*|\10\.0\.0\.0|" \
   ${KIBANA_HOME}/config/kibana.yml
 
 
