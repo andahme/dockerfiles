63746cfe31b637df31a30cf344c88de28feafc13
Andy Daniels <andy@andah.me>
2019-02-13 00:19:28 -0600

    Update jupyter Dockerfile and docker-entrypoint.sh

--- jupyter/docker-entrypoint.sh
+++ jupyter/docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo """
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.token = '${JUPYTER_TOKEN:-8888}'
-""" >> ${HOME}/.jupyter/jupyter_notebook_config.py
+""" > ${HOME}/.jupyter/jupyter_notebook_config.py
 
 
 exec $@
