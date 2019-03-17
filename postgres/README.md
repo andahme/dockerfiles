cd1bfd17eecad0f7b80be588a9c6712ae48bb1e4
Andy Daniels <andy@andah.me>
2019-03-17 16:08:51 -0500

    Simplify postgres docker-entrypoint.sh.

--- postgres/README.md
+++ postgres/README.md
@@ -2,20 +2,19 @@
 
 ### Postgres Server
 **NOTE**: Creates initial user (`postgres`) with password (`2345`).  
-**NOTE**: Creates an initial user database (`postgres`).  
+**NOTE**: Creates initial database (`postgres`).  
 **NOTE**: Publishes the database on (`5432`) of the localhost interface (`127.0.0.1`).  
+**NOTE**: Override database user with (`--env PGUSER=appuser`).  
+**NOTE**: Override database password with (`--env PGPASSWORD=apppass`).  
+**NOTE**: Override database name with (`--env PGDATABASE=appdb`).  
 ```bash
 docker run -d --name postgres \
-  --network ${NETWORK} \
   --publish 127.0.0.1:5432:5432 \
   andahme/postgres
 ```
 
 ### Postgres Client
-**NOTE**: The default host connection (`PGHOST`) has been pre-defined (`postgres`).   
 ```bash
-docker run -it --rm \
-  --network ${NETWORK} \
-  andahme/postgres psql
+docker exec -it postgres psql
 ```
 
--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -1,87 +1,18 @@
 #!/bin/bash -e
 
-COMMAND=$1; shift
-case ${COMMAND} in
-    init | initdb | postgres)
-        if [ ! -d ${PGDATA} ]; then
-            if [ -z "${INITDB_OPTIONS}" ]; then
-                if [ -t 0 ]; then
-                    INITDB_OPTIONS="--auth md5 --pwprompt"
-                else
-                    INITDB_OPTIONS="--auth md5 --pwfile=${INITDB_PWFILE:-/dev/shm/initdb_password}"
-                fi
+if [ $1 == "postgres" -a ! -d ${PGDATA} ]; then
+    if [ -z """${PGPASSWORD}""" -a -t 0 ]; then
+        initdb --auth md5 --username ${PGUSER:-postgres} --pwprompt
+    else
+        echo """${PGPASSWORD}""" > /tmp/pgpassword
+        initdb --auth md5 --username ${PGUSER:-postgres} --pwfile=/tmp/pgpassword
+        rm -f /tmp/pgpassword
+    fi
 
-                if [ -n "${PGUSER}" -a "${PGUSER}" != "postgres" ]; then
-                    INITDB_OPTIONS="--username ${PGUSER} ${INITDB_OPTIONS}"
-                fi
-            fi
+    if [ -n "${PGDATABASE}" -a "${PGDATABASE}" != "postgres" ]; then
+        echo "ALTER DATABASE postgres RENAME TO ${PGDATABASE};" | postgres --single template0 -F -c exit_on_error=true > /dev/null
+    fi
+fi
 
-            echo "ANDAHME Initializing Database - INITDB_OPTIONS (${INITDB_OPTIONS})"
-            echo """${INITDB_PASSWORD:-2345}""" > /dev/shm/initdb_password
-            initdb ${INITDB_OPTIONS}
-            rm -f /dev/shm/initdb_password
-
-            if [ -n "${PGDATABASE}" -a "${PGDATABASE}" != "postgres" ]; then
-                echo "ANDAHME Applying Configuration - PGDATABASE (${PGDATABASE})"
-                postgres --single template0 -F -c exit_on_error=true > /dev/null <<-EOF
-					ALTER DATABASE postgres RENAME TO ${PGDATABASE};
-					EOF
-            fi
-        fi
-        ;;&
-    init | initsql | postgres)
-        shopt -s nullglob
-        for DOCKER_INIT_SQL in /sql/*.sql; do
-            echo "ANDAHME Applying SQL (${DOCKER_INIT_SQL})"
-            postgres --single ${PGDATABASE:-postgres} -c exit_on_error=true -j >> ${PGDATA}/sql.log < ${DOCKER_INIT_SQL}
-        done
-        shopt -u nullglob
-        ;;&
-    init | initdb | initsql)
-        if [ -t 0 ]; then
-            echo "ANDAHME Tips - Get moving quickly with native tools"
-            echo
-            echo "    pg_ctl start                     (background)"
-            echo "      or"
-            echo "    postgres                         (foreground)"
-            echo
-        fi
-        ;;
-    noinit | postgres)
-        if [ -t 0 ]; then
-            echo "ANDAHME Tips - Getting outta here"
-            echo
-            echo "    ctrl-c                           (request shutdown)"
-            echo "      or"
-            echo "    ctrl-p ctrl-q                    (disconnect from container)"
-            echo
-        fi
-
-        exec postgres
-        ;;
-    bash)
-        echo
-        echo " ANDAHME - Bootstrap Tools"
-        echo
-        echo "    /docker-entrypoint.sh initdb"
-        echo "    /docker-entrypoint.sh initsql"
-        echo "    /docker-entrypoint.sh init       (initdb + initsql)"
-        echo
-        echo "    /docker-entrypoint.sh postgres   (initdb + initsql + postgres)"
-        echo
-        echo "    /docker-entrypoint.sh noinit     (only postgres)"
-        echo
-        echo
-        echo " ANDAHME - Environment"
-        echo
-        echo "    PGHOST                           (${PGHOST})"
-        echo "    PGDATABASE                       (${PGDATABASE:-default: postgres})"
-        echo "    PGUSER                           (${PGUSER:-default: postgres})"
-        echo
-        echo
-        ;;&
-    *)
-        exec ${COMMAND} $@
-        ;;
-esac
+exec $@
 
