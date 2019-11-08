82e80ebc5e552b6272b2b85fe82798c2184663f2
Andy Daniels <andy@andah.me>
2019-11-08 13:42:41 -0600

    Update kafka Dockerfile, docker-entrypoint.sh to add support for Kubernetes StatefulSets

--- kafka/README.md
+++ kafka/README.md
@@ -24,3 +24,40 @@ docker run -it --rm \
   andahme/kafka bash
 ```
 
+## k8s: Client
+
+#### Start Client
+```bash
+kubectl run \
+  -it --rm --restart Never \
+  --image registry.local.andah.me/kafka:local \
+  --env KAFKA_BOOTSTRAP_SERVER=kafka:9092 \
+  kafka-client -- bash
+```
+
+#### List Topics
+```bash
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVER} \
+  --list
+```
+
+#### Create Topic
+```bash
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVER} \
+  --create --topic <TOPIC> \
+  --partitions 1 \
+  --replication-factor 1
+```
+
+#### Console Consumer
+```bash
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVER} \
+  --list
+```
+
+#### Console Producer
+```bash
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVER} \
+  --list
+```
+
--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -e
 
-sed -i \
-  -e "s/^\\(broker.id=\\).*/\1${BROKER_ID:-1}/g" \
-  -e "s/^\\(zookeeper.connect=\\).*/\1${ZOOKEEPER_CONNECT:-zookeeper:2181}/g" \
-  ${KAFKA_HOME}/config/server.properties
+ORDNIAL_TEST='^[0-9]+$'
+
+if [ ${KAFKA_BROKER_ID} ]; then
+  echo "ANDAHME Initializing - KAFKA_BROKER_ID (${KAFKA_BROKER_ID})"
+  sed -i -e "s/^\(broker.id=\).*$/\1${KAFKA_BROKER_ID}/g" ${KAFKA_HOME}/config/server.properties
+elif [[ ${HOSTNAME##*-} =~ ${ORDINAL_TEST} ]]; then
+  echo "ANDAHME Initializing - KAFKA_BROKER_ID (${HOSTNAME##*-})"
+  sed -i -e "s/^\(broker.id=\).*$/\1${HOSTNAME##*-}/g" ${KAFKA_HOME}/config/server.properties
+fi
+
+echo "ANDAHME Initializing - KAFKA_ZOOKEEPER_CONNECT (${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181})"
+sed -i -e "s/^\(zookeeper\.connect=\).*/\1${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181}/g" ${KAFKA_HOME}/config/server.properties
 
 
 exec $@
