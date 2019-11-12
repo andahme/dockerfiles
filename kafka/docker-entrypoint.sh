5c03ece5d270fe2b0353cec1ff378af69941ad1b
Andy Daniels <andy@andah.me>
2019-11-12 10:48:01 -0600

    Update kafka docker-entrypoint.sh, remove stale docs

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -e
 
-ORDNIAL_TEST='^[0-9]+$'
 
 if [ ${KAFKA_BROKER_ID} ]; then
   echo "ANDAHME Initializing - KAFKA_BROKER_ID (${KAFKA_BROKER_ID})"
   sed -i -e "s/^\(broker.id=\).*$/\1${KAFKA_BROKER_ID}/g" ${KAFKA_HOME}/config/server.properties
-elif [[ ${HOSTNAME##*-} =~ ${ORDINAL_TEST} ]]; then
+elif [[ ${HOSTNAME##*-} =~ ^[0-9]+$ && ${HOSTNAME%-*} == $(hostname -f | cut -d'.' -f2) ]]; then
   echo "ANDAHME Initializing - KAFKA_BROKER_ID (${HOSTNAME##*-})"
   sed -i -e "s/^\(broker.id=\).*$/\1${HOSTNAME##*-}/g" ${KAFKA_HOME}/config/server.properties
 fi
 
-echo "ANDAHME Initializing - KAFKA_ZOOKEEPER_CONNECT (${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181})"
-sed -i -e "s/^\(zookeeper\.connect=\).*/\1${KAFKA_ZOOKEEPER_CONNECT:-zookeeper:2181}/g" ${KAFKA_HOME}/config/server.properties
+echo "ANDAHME Initializing - KAFKA_ZOOKEEPER_CONNECT (${KAFKA_ZOOKEEPER_CONNECT:=zookeeper:2181})"
+sed -i -e "s/^\(zookeeper\.connect=\).*/\1${KAFKA_ZOOKEEPER_CONNECT}/g" ${KAFKA_HOME}/config/server.properties
 
 
 exec $@
